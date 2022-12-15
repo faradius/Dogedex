8819ef11d1d8bf5fc3a5585cf6cd1e8a29898a07
Alejandro <faradius.magic187@gmail.com>
2022-12-15 16:25:45 -0600

    Validando los campos del sign up

--- app/src/main/java/com/alex/dogedex/auth/SignUpFragment.kt
+++ app/src/main/java/com/alex/dogedex/auth/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.alex.dogedex.auth
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,12 +12,56 @@ import com.alex.dogedex.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
 
+    private lateinit var binding: FragmentSignUpBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentSignUpBinding.inflate(inflater)
+        binding = FragmentSignUpBinding.inflate(inflater)
+        setupSignUpButton()
         return binding.root
+    }
+
+    private fun setupSignUpButton(){
+        binding.signUpButton.setOnClickListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        binding.emailInput.error = ""
+        binding.passwordInput.error = ""
+        binding.confirmPasswordInput.error = ""
+
+        val email = binding.emailEdit.text.toString()
+
+        if (!isValidEmail(email)){
+            binding.emailInput.error = getString(R.string.email_is_not_valid)
+            return
+        }
+
+        val password = binding.passwordEdit.text.toString()
+        if(password.isEmpty()){
+            binding.passwordInput.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        val passwordConfirmation = binding.confirmPasswordEdit.text.toString()
+        if(passwordConfirmation.isEmpty()){
+            binding.confirmPasswordInput.error = getString(R.string.password_must_not_be_empty)
+            return
+        }
+
+        if (password != passwordConfirmation){
+            binding.passwordInput.error = getString(R.string.passwords_do_not_match)
+            return
+        }
+    }
+
+    private fun isValidEmail(email: String?):Boolean{
+        //ya existe una función para vilidar un email
+        return !email.isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 
 }

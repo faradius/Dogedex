fe98c0fb52df8b51879f4aa90d2c173dd6742f0d
Alejandro <faradius.magic187@gmail.com>
2022-12-16 18:41:23 -0600

    Manejando excepciones al iniciar sesión y registro de usuario

--- app/src/main/java/com/alex/dogedex/api/makeNetworkCall.kt
+++ app/src/main/java/com/alex/dogedex/api/makeNetworkCall.kt
@@ -3,7 +3,11 @@ package com.alex.dogedex.api
 import com.alex.dogedex.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import java.net.UnknownHostException
+
+private const val UNAUTHORIZED_ERROR_CODE = 401
+private const val SHORT_PASSWORD_ERROR = 400
 
 suspend fun <T> makeNetworkCall(
     call: suspend () -> T
@@ -13,6 +17,15 @@ suspend fun <T> makeNetworkCall(
         ApiResponseStatus.Success(call())
     } catch (e: UnknownHostException) {
         ApiResponseStatus.Error(R.string.unknown_host_exception_error)
+    }catch (e: HttpException){
+        val errorMessage = if (e.code() == UNAUTHORIZED_ERROR_CODE){
+            R.string.wrong_user_or_password
+        }else if(e.code() == SHORT_PASSWORD_ERROR){
+            R.string.short_password
+        }else{
+            R.string.unknown_error
+        }
+        ApiResponseStatus.Error(errorMessage)
     } catch (e: Exception) {
         val errorMessage = when(e.message){
             "sign_up_error" -> R.string.error_sign_up

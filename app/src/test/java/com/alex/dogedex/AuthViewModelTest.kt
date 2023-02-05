a5b49c18bb2ee1d6f8a26a4d048636dbb7446332
Alejandro <faradius.magic187@gmail.com>
2023-02-05 00:49:35 -0600

    Creando testing a los viewModels

---
+++ app/src/test/java/com/alex/dogedex/AuthViewModelTest.kt
@@ -0,0 +1,78 @@
+package com.alex.dogedex
+
+import com.alex.dogedex.api.ApiResponseStatus
+import com.alex.dogedex.auth.AuthTasks
+import com.alex.dogedex.auth.AuthViewModel
+import com.alex.dogedex.model.User
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.junit.Test
+
+class AuthViewModelTest {
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var dogedexCoroutineRule = DogedexCoroutineRule()
+
+    @Test
+    fun test_login_validations_correct(){
+        class FakeAuthRepository: AuthTasks{
+            override suspend fun login(email: String, password: String): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(
+                    User(1,"alan@gmail.com","")
+                )
+            }
+
+            override suspend fun signUp(
+                email: String,
+                password: String,
+                passwordConfirmation: String
+            ): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(
+                    User(1,"","")
+                )
+            }
+
+        }
+
+        val viewModel = AuthViewModel(
+            authRepository = FakeAuthRepository()
+        )
+
+        viewModel.login("","test1234")
+        assertEquals(R.string.email_is_not_valid,viewModel.emailError.value)
+        viewModel.login("alan@gmail.com","")
+        assertEquals(R.string.password_must_not_be_empty,viewModel.passwordError.value)
+    }
+
+    @Test
+    fun test_login_states_correct(){
+        val fakeUser =  User(1,"alan@gmail.com","")
+
+        class FakeAuthRepository: AuthTasks{
+            override suspend fun login(email: String, password: String): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(fakeUser)
+            }
+
+            override suspend fun signUp(
+                email: String,
+                password: String,
+                passwordConfirmation: String
+            ): ApiResponseStatus<User> {
+                return ApiResponseStatus.Success(
+                    User(1,"","")
+                )
+            }
+
+        }
+
+        val viewModel = AuthViewModel(
+            authRepository = FakeAuthRepository()
+        )
+
+        viewModel.login("alan@gmail.com","test1234")
+        assertEquals(fakeUser.email, viewModel.user.value?.email)
+
+    }
+}
---
+++ app/src/test/java/com/alex/dogedex/DogListViewModelTest.kt
@@ -0,0 +1,104 @@
+package com.alex.dogedex
+
+import com.alex.dogedex.api.ApiResponseStatus
+import com.alex.dogedex.doglist.DogListViewModel
+import com.alex.dogedex.doglist.DogTasks
+import com.alex.dogedex.model.Dog
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Rule
+import org.junit.Test
+import org.junit.Assert.*
+
+
+class DogListViewModelTest {
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var dogedexCoroutineRule = DogedexCoroutineRule()
+
+    @Test
+    fun download_dogList_status_is_correct(){
+        class FakeDogRepository: DogTasks{
+            override suspend fun getDogCollection(): ApiResponseStatus<List<Dog>> {
+                return ApiResponseStatus.Success(
+                    listOf(
+                        Dog(1, 1, "", "", "", "", "", "", "", "", ""
+                            , inColletion = false),
+                        Dog(19, 2, "", "", "", "", "", "", "", "", ""
+                            , inColletion = false)
+                    )
+                )
+            }
+
+            override suspend fun addDogToUser(dogId: Long): ApiResponseStatus<Any> {
+                return ApiResponseStatus.Success(Unit)
+            }
+
+            override suspend fun getDogByMlId(mlDogId: String): ApiResponseStatus<Dog> {
+                return ApiResponseStatus.Success( Dog(1, 1, "", "", "", "", "", "", "", "", ""
+                    , inColletion = false))
+            }
+
+        }
+        //Primero se crea un objeto de la clase a testear
+        val viewModel = DogListViewModel(
+            dogRepository = FakeDogRepository()
+        )
+
+        assertEquals(2, viewModel.dogList.value.size)
+        //assertEquals(19, viewModel.dogList.value[1].id)
+        assert(viewModel.status.value is ApiResponseStatus.Success)
+    }
+
+    @Test
+    fun download_dogList_status_is_error(){
+        class FakeDogRepository: DogTasks{
+            override suspend fun getDogCollection(): ApiResponseStatus<List<Dog>> {
+                return ApiResponseStatus.Error(messageId = 12)
+            }
+
+            override suspend fun addDogToUser(dogId: Long): ApiResponseStatus<Any> {
+                return ApiResponseStatus.Success(Unit)
+            }
+
+            override suspend fun getDogByMlId(mlDogId: String): ApiResponseStatus<Dog> {
+                return ApiResponseStatus.Success( Dog(1, 1, "", "", "", "", "", "", "", "", ""
+                    , inColletion = false))
+            }
+
+        }
+
+        val viewModel = DogListViewModel(
+            dogRepository = FakeDogRepository()
+        )
+
+        assertEquals(0, viewModel.dogList.value.size)
+        assert(viewModel.status.value is ApiResponseStatus.Error)
+    }
+
+    @Test
+    fun reset_status_correct(){
+        class FakeDogRepository: DogTasks{
+            override suspend fun getDogCollection(): ApiResponseStatus<List<Dog>> {
+                return ApiResponseStatus.Error(messageId = 12)
+            }
+
+            override suspend fun addDogToUser(dogId: Long): ApiResponseStatus<Any> {
+                return ApiResponseStatus.Success(Unit)
+            }
+
+            override suspend fun getDogByMlId(mlDogId: String): ApiResponseStatus<Dog> {
+                return ApiResponseStatus.Success( Dog(1, 1, "", "", "", "", "", "", "", "", ""
+                    , inColletion = false))
+            }
+
+        }
+
+        val viewModel = DogListViewModel(
+            dogRepository = FakeDogRepository()
+        )
+
+        assert(viewModel.status.value is ApiResponseStatus.Error)
+        viewModel.resetApiResponseStatus()
+        assert(viewModel.status.value == null)
+    }
+}
